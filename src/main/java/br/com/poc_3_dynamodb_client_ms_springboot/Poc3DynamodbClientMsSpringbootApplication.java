afdb3fd0373c9b98f2259f15fc820174d75b4d5f
leandroltfx <leandroltfbr@hotmail.com>
2026-05-11 22:19:37 -0300

    Inicia o projeto

---
+++ src/main/java/br/com/poc_3_dynamodb_client_ms_springboot/Poc3DynamodbClientMsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.poc_3_dynamodb_client_ms_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poc3DynamodbClientMsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poc3DynamodbClientMsSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/poc_3_dynamodb_client_ms_springboot/Poc3DynamodbClientMsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.poc_3_dynamodb_client_ms_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Poc3DynamodbClientMsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
